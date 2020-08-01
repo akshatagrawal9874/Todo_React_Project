db0e67d9805a3c1176594d9dca804293c58ceec7
akshatagrawal9874 <akshatagrawal9874@gmail.com>
2020-08-01 14:44:40 +0530

    using css and makes its dynamic

--- src/App.js
+++ src/App.js
@@ -1,45 +1,114 @@
-import React from 'react';
-import todos from "./Todosdata"
-import TodoList from "./TodoList"
+import React from "react";
+import logo from "./LCO-logo-white.png";
+import "./App.css";
 
 class App extends React.Component {
-  constructor()
-  {
-    super()
-    this.state={
-      todos:todos
-    }
-    this.handleChange=this.handleChange.bind(this)
+  constructor(props) {
+    super(props);
+    this.state = {
+      newItem: "",
+      list: []
+    };
+    this.handleClick=this.handleClick.bind(this)
   }
-   
-   handleChange(id) {
+
+  addItem(todoValue) {
+    if (todoValue !== "") {
+      const newItem = {
+        id: Date.now(),
+        value: todoValue,
+        isDone: false
+      };
+      const list = [...this.state.list];
+      list.push(newItem);
+
+      this.setState({
+        list,
+        newItem: ""
+      });
+    }
+  }
+
+  deleteItem(id) {
+    const list = [...this.state.list];
+    const updatedlist = list.filter(item => item.id !== id);
+    this.setState({ list: updatedlist });
+  }
+
+  updateInput(input) {
+    this.setState({ newItem: input });
+  }
+  handleClick(id)
+  {
+    console.log("changed",id);
     this.setState(prevState => {
-        const updatedTodos = prevState.todos.map(todo => {
-            if (todo.id === id) {
-                return {
-                    ...todo,
-                    completed: !todo.completed
-                }
-            }
-            return todo
-        })
-        console.log(prevState.todos)
-        console.log(updatedTodos)
-        return {
-            todos: updatedTodos
-        }
-    })
-}
-  render(){
-   const todoscomponents= this.state.todos.map( task =><TodoList key={task.id} items={task} handleChange={this.handleChange}/>)
-  return (
-    <center>
-    <div className="container">
-    <h1>To do List</h1>
-    {todoscomponents}
-    </div>
-    </center>
-  );}
+      const updatedTodos = prevState.list.map(todo => {
+          if (todo.id === id) {
+              todo.isDone = !todo.isDone
+          }
+          return todo
+      })
+      return {
+          list: updatedTodos
+      }
+  })
+  }
+  render() {
+    const completedStyle = {
+      fontStyle: "italic",
+      color: "#cdcdcd",
+      textDecoration: "line-through"
+  }
+    return (
+      <div>
+        <img src={logo} width="100" height="100" className="logo" alt="Not Found"/>
+        <h1 className="app-title">ToDo App</h1>
+        <div className="container">
+          Add an Item....
+          <br />
+          <input
+            type="text"
+            className="input-text"
+            placeholder="Write a Todo"
+            required
+            value={this.state.newItem}
+            onChange={e => this.updateInput(e.target.value)}
+          />
+          <button
+            className="add-btn"
+            onClick={() => this.addItem(this.state.newItem)}
+            disabled={!this.state.newItem.length}
+          >
+            Add Todo
+          </button>
+          <div className="list">
+            <ul>
+              {this.state.list.map(item => {
+                return (
+                  <li key={item.id}>
+                    <div style={item.isDone ? completedStyle: null}>
+                    <input
+                      type="checkbox"
+                      name="idDone"
+                      checked={item.isDone}
+                      onChange={() => this.handleClick(item.id)}  
+                    />
+                      { item.value}
+                    <button
+                      className="btn"
+                      onClick={() => this.deleteItem(item.id)}
+                    >
+                      Delete
+                    </button></div>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
