88af2bc7eec6cb61cc318ee07320be3ab64a8d21
akshatagrawal9874 <akshatagrawal9874@gmail.com>
2020-08-06 20:14:37 +0530

    Updates

---
+++ precache-manifest.7254c2923e6d7df3c4d405b4e7fb2260.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d18863651d30b6211d9d9861aded0c0",
+    "url": "/Todo_React_Project/index.html"
+  },
+  {
+    "revision": "127a0751d0f6731e20d0",
+    "url": "/Todo_React_Project/static/css/main.37506711.chunk.css"
+  },
+  {
+    "revision": "8293b18d16089cf02c96",
+    "url": "/Todo_React_Project/static/js/2.aa0c3a9f.chunk.js"
+  },
+  {
+    "revision": "127a0751d0f6731e20d0",
+    "url": "/Todo_React_Project/static/js/main.c0f2ece8.chunk.js"
+  },
+  {
+    "revision": "9ef8cb9eec37b7724af7",
+    "url": "/Todo_React_Project/static/js/runtime~main.f82961e6.js"
+  },
+  {
+    "revision": "93326a916609381db6a9b85ae83f3f7f",
+    "url": "/Todo_React_Project/static/media/LCO-logo-white.93326a91.png"
+  }
+]);
